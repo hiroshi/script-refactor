164dfc9c1a272a970e4d18a98e5f242d13ec2b9a
hiroshi <hiroshi3110@gmail.com>
2009-01-26 22:24:42 -0700

    implement install.rb: generate script/refactor

--- install.rb
+++ install.rb
@@ -1,1 +1,8 @@
-# Install hook code here
+# place script/refactor
+open(File.dirname(__FILE__) + "../../script/refactor") do |file|
+  file.print <<-FILE
+#!/usr/bin/env ruby                                                                                                                     
+require File.dirname(__FILE__) + '/../vendor/plugins/script_refactor/lib/script_refactor'
+  FILE
+end
+
