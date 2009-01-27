062cff4e506f00914cba60978ff7a169905eea59
Hiroshi Saito <hiroshi3110@gmail.com>
2009-01-27 14:33:09 +0900

    fix install.rb

--- install.rb
+++ install.rb
@@ -1,8 +1,8 @@
-# place script/refactor
-open(File.dirname(__FILE__) + "../../script/refactor") do |file|
-  file.print <<-FILE
-#!/usr/bin/env ruby                                                                                                                     
+# install script/refactor
+open(File.dirname(__FILE__) + "/../../../script/refactor", "w") do |file|
+  file.print <<-SCRIPT
+#!/usr/bin/env ruby
 require File.dirname(__FILE__) + '/../vendor/plugins/script_refactor/lib/script_refactor'
-  FILE
+  SCRIPT
+  file.chmod(0755) # make executable
 end
-
